d682f9ab3c16580f6cbf69b0f5a12b0daff67de8
sevval <sevvallkucukk61@gmail.com>
2020-06-20 13:36:23 +0300

    konum alma baslatan ve bitiren fonksiyonlar eklendi. sinyali yüzdelik olarak ceviren kod eklendi.

--- app/MainScreen.js
+++ app/MainScreen.js
@@ -99,53 +99,111 @@ export class MainScreen extends Component {
         });
     }
 
+    // https://github.com/Agontuk/react-native-geolocation-service#watchpositionsuccesscallback-errorcallback-options
     getLocationUpdates = async () => {
         //const hasLocationPermission = await this.hasLocationPermission();
 
         // if (!hasLocationPermission) {
         //   return;
         // }
+        console.log("getLocationUpdates()")
 
-        this.setState({ updatesEnabled: true }, () => {
-            this.watchId = Geolocation.watchPosition(
-                (position) => {
-                    this.setState({
-                        acc: position.coords.accuracy,
-                        alt: position.coords.altitude,
-                        lat: position.coords.latitude,
-                        lon: position.coords.longitude,
-                    });
-                    console.log(position);
-                },
-                (error) => {
-                    console.log(error);
-                },
-                {
-                    enableHighAccuracy: true,
-                    distanceFilter: 10,
-                    interval: 1000 * this.state.period,
-                    fastestInterval: 2000,
-                    forceRequestLocation: true,
-                    showLocationDialog: true,
-                    useSignificantChanges: false,
-                },
-            );
-        });
-    };
+        this.setState({ updatesEnabled: true })
 
+        this.watchId = Geolocation.watchPosition(
+            (position) => {
+                // telefon konum bilgilerini verir.
+                this.setState({
+                    acc: position.coords.accuracy,
+                    alt: position.coords.altitude,
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude,
+                });
+                console.log("postion: ", position);
+
+                let record = {}
+                record["accuracy"] = position.coords.accuracy;
+                record["location"] = new firestore.GeoPoint(position.coords.latitude, position.coords.longitude);
+                record["altitude"] = position.coords.altitude;
+                record["time"] = Moment(position.timestamp).format('YYYY-MM-DD_HH:MM:SS');
+
+                // telefon ağ isim bilgisini verir. 2g/3g/4g gibi
+                TelephonyManager.getNetworkClass((network) => {
+                    if (network != null) {
+                        console.log("ağ tipi: ", network);
+                        record["network"] = network;
+                        this.setState({ net: network });
+                    }
+                });
+
+
+                // telefon sinyal bilgilerini verir. ağ tipini verir. LTE/WCDMA/UTMS. sinyal gücü verir 18asu.
+                TelephonyManager.getCellInfo((network) => {
+
+                    //bazen telefon cekmez bu değer bos döner.
+                    if (network[0].connectionType = ! null) {
+                        record["type"] = network[0].connectionType;
+                        console.log("connectionType \t" + network[0].connectionType);
+                        this.setState({ typ: network[0].connectionType });
+                    } else {
+                        record["type"] = "no_signal";
+                        console.log("telefon cekmiyor");
+                    }
+
+                    // bazen telefon cekmez, bu deger boş dönebilir.
+                    if (network[0].cellSignalStrength.asuLevel) {
+                        const strength = this.convertPercentage(network[0].cellSignalStrength.asuLevel);
+
+                        record["strength"] = strength;
+                        record["asuLevel"] = network[0].cellSignalStrength.asuLevel;
+
+                        this.setState({ str: strength });
+
+                        console.log("asuLevel \t" + network[0].cellSignalStrength.asuLevel);
+                    }
+
+                });
+
+                console.log('record: ', record);
+
+                this.addRecord(record);
+
+                //this.readRecords();
+
+            },
+            (error) => {
+                console.log('location err: ', error);
+            },
+            {
+                enableHighAccuracy: true,
+                distanceFilter: 0,
+                interval: 1000 * this.state.period,
+                fastestInterval: 2000,
+                forceRequestLocation: true,
+                showLocationDialog: true,
+                useSignificantChanges: false,
+            },
+        );
+    }
+
+    // konum alma ve kayıt etmeyi durdurur.
     removeLocationUpdates = () => {
+        console.log("removeLocationUpdates()")
         if (this.watchId !== null) {
             Geolocation.clearWatch(this.watchId);
+            Geolocation.stopObserving();
             this.setState({ updatesEnabled: false });
         }
     };
-
-    start() {
-        alert('kayıt başlıyor');
-    }
-
-    stop() {
-        alert('kayıt bitiyor');
+    
+    // sinyal gücünü asu degerinden yüzdelik değere cevirir.
+    // 4g icin hesaplama ile diğer ağlar icin yapılan farklıdır.
+    convertPercentage(asu) {
+        if (type == '4g') {
+            return ((asu - 3) / 92) * 100;
+        } else {
+            return (asu / 32) * 100;
+        }
     }
 
     saveConfig() {
@@ -160,82 +218,79 @@ export class MainScreen extends Component {
     render() {
         return (
             <>
-            <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}>
+                <SafeAreaView>
+                    <ScrollView
+                        contentInsetAdjustmentBehavior="automatic"
+                        style={styles.scrollView}>
 
-                    <View style={styles.header}>
-                        <Text> SİNYAL GÜCÜ </Text>
-                    </View>
+                        <View style={styles.header}>
+                            <Text> SİNYAL GÜCÜ </Text>
+                        </View>
 
-                    <View style={styles.row}>
+                        <View style={styles.row}>
 
-                        <Button title="config" onPress={() => this.setState({ modal0: true })} />
+                            <Button title="config" onPress={() => this.setState({ modal0: true })} />
 
-                    </View>
+                        </View>
+                        <View style={styles.row}>
+                            <Button
+                                onPress={() => this.getLocationUpdates()}
+                                title="Başlat"
+                                color={this.state.updatesEnabled ? 'green' : 'blue'}
+                            />
+                            <Button
+                                onPress={() => this.removeLocationUpdates()}
+                                title="Bitir"
+                                color={this.state.updatesEnabled ? 'blue' : 'green'}
+                            />
+                        </View>
 
-                    <View style={styles.row}>
-                        <Button
-                            onPress={this.getLocationUpdates}
-                            title="Başlat"
-                        />
-                        <Button
-                            onPress={this.removeLocationUpdates}
-                            title="Bitir"
-                        />
-                    </View>
-                    <View style={styles.row}>
-                        <Text>enlem: {this.state.lat} </Text>
-                        <Text>boylam: {this.state.lon} </Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text>sinyal gücü: {this.state.strength} </Text>
-                    </View>
+                        <View style={styles.row}>
+                            <Text>enlem: {this.state.lat} </Text>
+                            <Text>boylam: {this.state.lon} </Text>
+                        </View>
 
-                    <View style={styles.row}>
-                        <Button
-                            onPress={() => this.export()}
-                            title="Export"
-                        />
-                    </View>
+                        <View style={styles.row}>
+                            <Text>ağ tipi: {this.state.net} </Text>
+                            <Text>sinyal gücü: {this.state.str} </Text>
+                        </View>
 
-                    <View style={styles.row}>
-                        <Button
-                            onPress={() => this.export()}
-                            title="Upload"
-                        />
-                    </View>
+                        <View style={styles.row}>
+                            <Button
+                                onPress={() => this.export()}
+                                title="Export"
+                            />
+                        </View>
 
-                </ScrollView>
-            </SafeAreaView>
+                    </ScrollView>
+                </SafeAreaView>
 
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={this.state.modal0}
-                onRequestClose={() => this.setState({ modal0: false })} 
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={this.state.modal0}
+                    onRequestClose={() => this.setState({ modal0: false })}
                 >
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                        <TextInput
-                            value={this.state.period}
-                            onChangeText={(period) => this.setState({ period })}
-                            style={styles.inputs}
-                            placeholder={"örn. 2sn"}
-                            placeholderTextColor={"#aaa"}
-                            maxLength={3}
-                            autoCapitalize="none"
-                            textContentType="telephoneNumber"
-                            returnKeyType="done"
-                            keyboardType="number-pad"
-                            blurOnSubmit={false}
-                        />
-                        <Button title="Ayarları Kaydet" onPress={() => this.saveConfig()} />
-                        <Button title="close" onPress={() => this.setState({ modal0: false })} />
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <TextInput
+                                value={this.state.period}
+                                onChangeText={(period) => this.setState({ period })}
+                                style={styles.inputs}
+                                placeholder={"örn. 2sn"}
+                                placeholderTextColor={"#aaa"}
+                                maxLength={3}
+                                autoCapitalize="none"
+                                textContentType="telephoneNumber"
+                                returnKeyType="done"
+                                keyboardType="number-pad"
+                                blurOnSubmit={false}
+                            />
+                            <Button title="Ayarları Kaydet" onPress={() => this.saveConfig()} />
+                            <Button title="close" onPress={() => this.setState({ modal0: false })} />
+                        </View>
                     </View>
-                </View>
-            </Modal>
+                </Modal>
 
             </>
         )
